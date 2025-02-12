15a8e7031a3930e13986435a21d308158be9987b
Wellison Oliveira <manofama@gmail.com>
2025-02-12 14:53:44 +0000

    Atualizando API para produção

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://SEU_PROJETO.railway.app/api", // Troque pela URL real da sua API
+  baseURL: "https://suinobackend.onrender.com", // Troque pela URL real da sua API
 });
 
 export default api;
